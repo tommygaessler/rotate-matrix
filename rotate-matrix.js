cd03e85075cfb527baedc00af7ae16d5cc366d1c
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-09-06 09:30:41 -0600

    finished

---
+++ rotate-matrix.js
@@ -0,0 +1,22 @@
+var input = [ [29, 62, 8], [12, 60, 31], [99, 79, 12] ];
+
+function rotateMatrix(array) {
+
+  var output = [];
+
+  for (var i = 0; i < array.length; i++) {
+    output.push([]);
+    console.log(output);
+  }
+
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < 3; j++) {
+      output[j].unshift(array[i][j]);
+      console.log(i, j);
+      console.log(output);
+    }
+  }
+
+  return output;
+}
+console.log(rotateMatrix(input));
